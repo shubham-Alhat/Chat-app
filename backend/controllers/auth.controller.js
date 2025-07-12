a9abea055757b3f99d25343aa5f4bc90ae4c8256
shubham-Alhat <shubhamalhat520@gmail.com>
2025-07-12 08:31:51 +0530

    user controller - signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,32 @@
+import bcrypt from "bcryptjs";
+import { User } from "../models/user.model.js";
+
 export const signup = async (req, res) => {
+  const { username, fullName, email, password } = req.body;
   try {
+    if (!username || !email || !password || !fullName) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required!" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be atleast 6 characters",
+        success: false,
+      });
+    }
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      username,
+      fullName,
+      password: hashedPassword,
+      email,
+    });
   } catch (error) {}
 };
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "../routes/auth.route.js";
 import dotenv from "dotenv";
 import connectToDb from "../db/connection.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,6 +12,9 @@ const PORT = process.env.PORT;
 
 // routes
 app.use("/api/v1/auth", authRoutes);
+app.use(express.json({ limit: "16kb" })); // allow express to take json data
+app.use(cookieParser()); // allow us to set and get user browser cookies
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Allow express to encode the url. eg " " = %20 or +. @ = %40
 
 connectToDb()
   .then(() => {
