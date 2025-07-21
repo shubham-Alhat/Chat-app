13c5dde57715ba5793248bd8c8357bed695faa4d
shubham-Alhat <shubhamalhat520@gmail.com>
2025-07-21 09:12:31 +0530

    fix: update Navbar background style for improved visibility

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,7 +41,7 @@ function Navbar() {
     <>
       <header
         className=" border-b border-base-300 fixed w-full top-0 z-40 
-    backdrop-blur-lg bg-base-100 bg-base-100/80"
+    backdrop-blur-lg bg-base-100"
       >
         <div className="container mx-auto px-4 h-16">
           <div className="flex items-center justify-between h-full">
