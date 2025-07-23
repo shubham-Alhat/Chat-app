8c11df193f232d27f029bdb378ce5f8c87e386be
shubham-Alhat <shubhamalhat520@gmail.com>
2025-07-23 22:55:23 +0530

    fix: update message routes for clarity and consistency

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -12,9 +12,14 @@ const router = express.Router();
 
 router.route("/users").get(verifyJwt, getUsersForSidebar);
 
-router.route("/:id").get(verifyJwt, getMessages);
+// router.route("/:id").get(verifyJwt, getMessages);
+router.get("/:id", verifyJwt, getMessages);
+router.post("/send/:id", verifyJwt, upload.single("file"), sendMessage);
+router.delete("/delete/:messageId", verifyJwt, deleteMessage);
 
-router.route("/send/:id").post(verifyJwt, upload.single("file"), sendMessage);
-router.route("/delete/:messageId").delete(verifyJwt, deleteMessage);
+router.get("/users", protectRoute, getUsersForSidebar); // <----
+
+// router.route("/send/:id").post(verifyJwt, upload.single("file"), sendMessage);
+// router.route("/delete/:messageId").delete(verifyJwt, deleteMessage);
 
 export default router;
